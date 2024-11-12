2b9d8ac5abfab061a14b121b5900f4cb8365bcf8
GoulartNogueira <goulartnogueira@gmail.com>
2024-11-12 19:08:43 -0300

    Fix sizes. Update README.md and app.py to enhance star rating feature and examples

--- README.md
+++ README.md
@@ -2,28 +2,13 @@
 
 Generates a dynamic star rating image (SVG) using Python, which is deployed freely via Vercel.
 
-![Star Rating](https://starrating-beta.vercel.app/1.0/) ![Star Rating](https://starrating-beta.vercel.app/3.5/) ![Star Rating](https://starrating-beta.vercel.app/5.0/)
-
 Inspired by [![guibranco/progressbar](https://img.shields.io/badge/guibranco%2Fprogressbar-black?style=flat&logo=github)](https://github.com/guibranco/progressbar).
-
----
-
-## Usage
 
 This service is deployed on [Vercel](https://vercel.com) and accessible via the domain [starrating-beta.vercel.app](https://starrating-beta.vercel.app).
 
 ---
 
-## Parameters
-
-| Parameter | Description                                   | Default Value |
-| --------- | --------------------------------------------- | ------------- |
-| `rating`  | The rating value to display (between 0 and 5) | 0             |
-| `size`    | The size of each star in pixels               | 24            |
-
----
-
-## Example
+## Usage
 
 To display a star rating of 2.7, use the following URL:
 
@@ -34,3 +19,31 @@ This will generate an SVG image showing a star rating of 2.7 out of 5.
 To display a star rating of 2.7 with a star size of 48 pixels, use the following URL:
 
 `https://starrating-beta.vercel.app/2.7/?size=48`
+
+## Examples
+
+| Rating | Star Rating Image                                       | Markdown                                                  |
+| ------ | ------------------------------------------------------- | --------------------------------------------------------- |
+| 1.0    | ![Star Rating](https://starrating-beta.vercel.app/1.0/) | `![Star Rating](https://starrating-beta.vercel.app/1.0/)` |
+| 2.6    | ![Star Rating](https://starrating-beta.vercel.app/2.6/) | `![Star Rating](https://starrating-beta.vercel.app/2.6/)` |
+| 3.3    | ![Star Rating](https://starrating-beta.vercel.app/3.3/) | `![Star Rating](https://starrating-beta.vercel.app/3.3/)` |
+| 4.0    | ![Star Rating](https://starrating-beta.vercel.app/4.0/) | `![Star Rating](https://starrating-beta.vercel.app/4.0/)` |
+| 5.0    | ![Star Rating](https://starrating-beta.vercel.app/5.0/) | `![Star Rating](https://starrating-beta.vercel.app/5.0/)` |
+
+---
+
+## Parameters
+
+| Parameter | Description                                   | Default Value |
+| --------- | --------------------------------------------- | ------------- |
+| `rating`  | The rating value to display (between 0 and 5) | 0             |
+| `size`    | The size of each star in pixels               | 24            |
+
+| Rating | Size | Star Rating Image                                  | Markdown                                                          |
+| ------ | ---- | -------------------------------------------------- | ----------------------------------------------------------------- |
+| 3.3    | 6    | ![Star Rating](http://127.0.0.1:5000/3.3/?size=6)  | `![Star Rating](https://starrating-beta.vercel.app/3.3/?size=6)`  |
+| 3.3    | 12   | ![Star Rating](http://127.0.0.1:5000/3.3/?size=12) | `![Star Rating](https://starrating-beta.vercel.app/3.3/?size=12)` |
+| 3.3    | 24   | ![Star Rating](http://127.0.0.1:5000/3.3/?size=24) | `![Star Rating](https://starrating-beta.vercel.app/3.3/?size=24)` |
+| 3.3    | 32   | ![Star Rating](http://127.0.0.1:5000/3.3/?size=32) | `![Star Rating](https://starrating-beta.vercel.app/3.3/?size=32)` |
+| 3.3    | 48   | ![Star Rating](http://127.0.0.1:5000/3.3/?size=48) | `![Star Rating](https://starrating-beta.vercel.app/3.3/?size=48)` |
+| 3.3    | 96   | ![Star Rating](http://127.0.0.1:5000/3.3/?size=96) | `![Star Rating](https://starrating-beta.vercel.app/3.3/?size=96)` |
--- app.py
+++ app.py
@@ -2,69 +2,57 @@ from flask import Flask, make_response, redirect, render_template, request
 
 app = Flask(__name__)
 
-def is_true(value):
-  return value.lower() == 'true'
-
 def get_star_rating(rating, size=24):
-    """Get the star rating representation of progress based on a scale.
-
-    This function calculates the ratio of progress to scale and returns an
-    SVG representation of the star rating. The star rating is defined as
-    follows: - 1 star for progress less than 20% - 2 stars for progress
-    between 20% and 40% - 3 stars for progress between 40% and 60% - 4 stars
-    for progress between 60% and 80% - 5 stars for progress of 80% or more.
-
+    """Get the star rating representation.
     Args:
         rating (float): The rating value between 0 and 5.
         size (int): The size of each star in pixels.
-
     Returns:
         str: An SVG representation of the star rating.
     """
-
     rating = max(0, min(5, rating))
     full_stars = int(rating)
     partial = rating - full_stars
     empty_stars = 5 - full_stars - (1 if partial > 0 else 0)
-
-    star_svg = f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {size * 5} {size}" width="{size * 5}" height="{size}">'
+    
+    # Using a 0-100 coordinate system for the star path
+    star_path = "M50 5L61.5 31.5L90.5 35.5L70 55.5L75.5 84L50 70.5L24.5 84L30 55.5L9.5 35.5L38.5 31.5Z"
+    
+    star_svg = f'''<svg xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 500 100" 
+        width="{size * 5}" 
+        height="{size}">'''
     
     # Add full stars
     for i in range(full_stars):
-        x = i * size
+        x = i * 100
         star_svg += f'''
-        <path transform="translate({x},0)" fill="gold" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
-        '''
+            <path transform="translate({x},0)" fill="gold" d="{star_path}"/>'''
     
     # Add partial star if needed
     if partial > 0:
-        x = full_stars * size
+        x = full_stars * 100
         star_svg += f'''
-        <defs>
-            <clipPath id="partial">
-                <rect x="0" y="0" width="{partial * size}" height="{size}" />
-            </clipPath>
-        </defs>
-        <path transform="translate({x},0)" fill="#ddd" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
-        <path transform="translate({x},0)" fill="gold" clip-path="url(#partial)" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
-        '''
+            <defs>
+                <clipPath id="partial">
+                    <rect x="0" y="0" width="{partial * 100}" height="100" />
+                </clipPath>
+            </defs>
+            <path transform="translate({x},0)" fill="#ddd" d="{star_path}"/>
+            <path transform="translate({x},0)" fill="gold" clip-path="url(#partial)" d="{star_path}"/>'''
     
     # Add empty stars
     for i in range(empty_stars):
-        x = (full_stars + 1) * size if partial > 0 else full_stars * size
+        x = (full_stars + (1 if partial > 0 else 0) + i) * 100
         star_svg += f'''
-        <path transform="translate({x},0)" fill="#ddd" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
-        '''
-        full_stars += 1
-
+            <path transform="translate({x},0)" fill="#ddd" d="{star_path}"/>'''
+    
     star_svg += '</svg>'
     return star_svg
-
 
 @app.route('/')
 def redirect_to_github():
     return redirect("https://github.com/GoulartNogueira/Star-Rating/", code=302)
-
 
 @app.route('/<rating>/')
 def star_rating(rating):
@@ -72,7 +60,7 @@ def star_rating(rating):
         rating = float(rating)
     except ValueError:
         return "Invalid rating value", 400
-
+    
     size = request.args.get('size', default=24, type=int)
     svg = get_star_rating(rating, size)
     response = make_response(svg)
